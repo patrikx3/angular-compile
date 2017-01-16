eaeb0a622ffd36c9577ff3e6b4db63a6bda2fe92
Patrik Laszlo <alabard@gmail.com>
2017-01-16 20:53:34 +0100

    p3x-ng2-compile-html patrik laszlo

--- test/angular2/app/Page.ts
+++ test/angular2/app/Page.ts
@@ -11,8 +11,9 @@ import {CompileHtmlService } from '../../../src';
 @Component({
     selector: 'p3x-ng2-compile-html-text',
     template: `
-    <div #container>loading ...</div>
-    <div [p3xCompileHtml]="data" [p3xCompileHtmlRef]="ref">loading ...</div>
+    <div #container></div>
+    <hr/>
+    <div [p3xCompileHtml]="data2" [p3xCompileHtmlRef]="ref"></div>
 `,
 })
 @Injectable()
@@ -20,33 +21,42 @@ export class Page implements OnInit {
 
     @ViewChild('container', {read: ViewContainerRef}) container: ViewContainerRef;
 
-    data: string;
+    data1: string;
+    data2: string;
 
     ref: Page;
 
-    counter : number = 0;
+    counter1 : number = 0;
+    counter2 : number = 0;
 
     constructor( private compileHtmlService: CompileHtmlService ) {
         this.ref = this;
-        this.update();
     }
 
-    private update() {
-        this.counter = this.counter + 1;
-        this.data = `<div>Done</div><a href="javascript:void(0);" (click)="ref.alert('ok')">If click works it says OK!</a>${this.counter}`;
-    }
-
-    alert(string: string ) {
-        alert(string);
-        this.update();
-    }
-
-    ngOnInit() {
+    private update1() {
+        this.counter1++;
+        this.data1 = `
+<div>Service</div><a href="javascript:void(0);" (click)="ref.update1()">Click me via a service!</a>
+<div>${this.counter1}</div>
+`;
         this.compileHtmlService.compile({
-            template: this.data,
+            template: this.data1,
             container: this.container,
             ref: this,
         })
+    }
+
+    private update2() {
+        this.counter2++;
+        this.data2 = `
+<div>Attribute</div><a href="javascript:void(0);" (click)="ref.update2()">Click me via a service!</a>
+<div>${this.counter2}</div>
+`;
+    }
+
+    ngOnInit() {
+        this.update1();
+        this.update2();
     }
 
 }
