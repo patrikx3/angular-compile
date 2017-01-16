976a970cc1d6a8b9aa85df9252aadeefdd9b97b3
Patrik Laszlo <alabard@gmail.com>
2017-01-16 19:11:35 +0100

    p3x-ng2-compile-html patrik laszlo

--- src/CompileHtmlAttribute.ts
+++ src/CompileHtmlAttribute.ts
@@ -1,21 +1,29 @@
-import {Directive, Input, Injectable, ViewContainerRef, OnInit} from '@angular/core';
+import {Directive, Input, Injectable, ViewContainerRef, OnInit, OnChanges, SimpleChanges} from '@angular/core';
 
 import { CompileHtmlService } from './CompileHtmlService';
 
 @Directive({ selector: '[p3xCompileHtml]' })
 @Injectable()
-export class CompileHtmlAttribute {
+export class CompileHtmlAttribute implements OnInit, OnChanges{
 
     @Input('p3xCompileHtml') p3xHtml: string;
 
     @Input() p3xCompileHtmlRef: any;
 
-    ngOnInit() {
+    update() {
         this.service.compile({
             template: this.p3xHtml,
             container: this.container,
             ref: this.p3xCompileHtmlRef
         })
+    }
+
+    ngOnInit() {
+        this.update();
+    }
+
+    ngOnChanges(changes: SimpleChanges) {
+        this.update();
     }
 
     constructor(
--- test/angular2/app/Page.ts
+++ test/angular2/app/Page.ts
@@ -20,16 +20,25 @@ export class Page implements OnInit {
 
     @ViewChild('container', {read: ViewContainerRef}) container: ViewContainerRef;
 
-    data: string = `<div>Done</div><a href="javascript:void(0);" (click)="ref.alert('ok')">If click works it says OK!</a>`;
+    data: string;
 
     ref: Page;
 
+    counter : number = 0;
+
     constructor( private compileHtmlService: CompileHtmlService ) {
         this.ref = this;
+        this.update();
+    }
+
+    private update() {
+        this.counter = this.counter + 1;
+        this.data = `<div>Done</div><a href="javascript:void(0);" (click)="ref.alert('ok')">If click works it says OK!</a>${this.counter}`;
     }
 
     alert(string: string ) {
         alert(string);
+        this.update();
     }
 
     ngOnInit() {
